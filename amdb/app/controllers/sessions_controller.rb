a30a5f4ddc936f741906f49651ec8e480c84de30
Raghu Betina <rbetina@gmail.com>
2013-05-13 19:27:32 -0500

    Completed authentication logic in the sessions create action.

--- amdb/app/controllers/sessions_controller.rb
+++ amdb/app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:email])
+
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to movies_url, notice: 'Signed in successfully.'
+    else
+      render 'new'
+    end
   end
 
   def destroy
